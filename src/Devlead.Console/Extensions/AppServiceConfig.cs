b40cd57733cb9acbb1789aefa060e10fc8d29c65
Mattias Karlsson <matkar@wcom.se>
2025-01-20 15:56:51 +0100

    Add AppServiceConfig AddCommand<TCommand> helper

--- src/Devlead.Console/Extensions/AppServiceConfig.cs
+++ src/Devlead.Console/Extensions/AppServiceConfig.cs
@@ -13,7 +13,18 @@ public record AppServiceConfig(CommandApp App, IServiceCollection Services, ICon
 /// <param name="Services">The service collection.</param>
 /// <param name="Configurator">The command configurator with settings.</param>
 public record AppServiceConfig<TSettings>(CommandApp App, IServiceCollection Services, IConfigurator<TSettings> Configurator)
-    where TSettings : CommandSettings; 
+    where TSettings : CommandSettings
+{
+    /// <summary>
+    /// Adds a command with the specified settings type.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of command to add.</typeparam>
+    /// <param name="name">The name of the command.</param>
+    /// <returns>The app service command configuration.</returns>
+    public AppServiceCommandConfig AddCommand<TCommand>(string name)
+        where TCommand : class, ICommandLimiter<TSettings>
+        => this.AddCommand<TCommand, TSettings>(name);
+}
 
 /// <summary>
 /// Represents the configuration for application services with command configuration.
